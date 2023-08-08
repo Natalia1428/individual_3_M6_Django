f263f3c148ab8a56892f4b40a33a4cea2694e0f5
Natalia1428 <naty.ponce5@gmail.com>
2023-08-08 11:10:22 -0400

    páginas de index y usuarios

--- proyecto/app/views.py
+++ proyecto/app/views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render
+from .models import Usuario
 
-# Create your views here.
+
 def vista_index(request):
     return render(request, 'index.html') 
+
+def vista_usuarios(request):
+    usuarios = Usuario.objects.all()
+    contexto = {
+        'usuarios' : usuarios
+    }
+
+    return render(request, 'usuarios.html', contexto)
